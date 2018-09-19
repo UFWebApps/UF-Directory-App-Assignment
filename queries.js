44d963cbab36855c19a140ca9453136b12afe637
595456852 <42814114+595456852@users.noreply.github.com>
2018-09-18 23:24:15 -0400

    Update queries.js

--- queries.js
+++ queries.js
@@ -5,6 +5,11 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.find({ code: 'LBWEST' , name : 'Library West'}, function(err,listing) {
+      if (err) throw err;
+
+      console.log(listing);
+  });
 };
 var removeCable = function() {
   /*
@@ -12,20 +17,41 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+    Listing.findOneAndRemove({ code : 'CABL' }, function(err) {
+        if (err) throw err;
+
+        console.log('CABL deleted!');
+    });
 };
-var updatePhelpsLab = function() {
-  /*
-    Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
+var updatePhelpsMemorial = function() {
+    /*
+    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+    Listing.find({name : 'Phelps Memorial Hospital Center'}, function(err, listing) {
+        if (err) throw err;
+
+        listing.address = 'Gainesville, FL';
+
+        listing.save(function(err) {
+            if (err) throw err;
+
+            console.log('Listing successfully updated!');
+        });
+    });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find( {}, function(err, listings) {
+      if (err) throw err;
+
+      console.log(listings);
+  });
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsLab();
+updatePhelpsMemorial();
 retrieveAllListings();
